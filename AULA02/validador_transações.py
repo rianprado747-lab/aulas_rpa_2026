b362b6975042c6db7d87cd5153e45c2f6657c74a
Rian Prado <pradorian747@gmail.com>
2026-08-26 00:42:29 -0300

    lab02: entrega RA 6326066

---
+++ AULA02/validador_transações.py
@@ -0,0 +1,15 @@
+transacoes = [150.0, 3200.5, 12500.0, 450.0, -50.0, 800.0, 0]
+
+for valor in transacoes:
+    print()
+    if valor > 10000:
+        print(f'\033[33m[ALERTA] Transação suspeita de R${valor:.2f}: Encaminhada para auditoria.\033[0m')
+        continue
+    elif valor <= 0:
+        if valor == 0:
+            print(f'\033[31m[ERRO CRÍTICO] Transação inválida encontrada de R${valor:.2f}: Valor negativo não permitido.\033[0m')
+            break
+        else:
+            print(f'\033[31m[ERRO CRÍTICO] Transação inválida encontrada de R${valor:.2f}: Valor negativo não permitido.\033[0m')
+    else:
+        print(f'\033[32mTransação de R${valor:.2f} processada com sucesso.\033[0m')
